19fde0ce6748db70be9a717e7e8e1bc571ec10f9
Coral Bunty <brightalour@gmail.com>
2024-08-13 20:39:54 +0000

    setup gemini-pro api

--- app.py
+++ app.py
@@ -3,9 +3,7 @@ from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.schema import HumanMessage
 
-
-load_dotenv()
-api_key = os.getenv("GOOGLE_API_KEY")
+os.environ["GOOGLE_API_KEY"] = "AIzaSyDSjM13zy6Pv5Cm47Hk7va3XF7YJKYHBNA"
 
 llm = ChatGoogleGenerativeAI(model="gemini-pro")
 result = llm.invoke("Write a ballad about Gemini Pro in around 3 sentences.")
